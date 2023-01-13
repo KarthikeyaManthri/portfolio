29af689661b5860048d454124a214aeb4733862d
KarthikeyaManthri <30819200+KarthikeyaManthri@users.noreply.github.com>
2023-01-13 07:44:07 +0000

    Deploying to gh-pages from @ KarthikeyaManthri/portfolio@887000e76b35d1c8ac208d45af0df99bc7b359dc 🚀

---
+++ _next/static/ROzergJXULVqGMppO9wXT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/326-2cbe98dbb9e3b161.js","static/chunks/pages/index-a4380ea6d26dbb76.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
